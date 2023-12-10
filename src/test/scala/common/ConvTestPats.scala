432352a387963eaac30d10e21a49430587a89420
Kazutomo Yoshii <kazutomo.yoshii@gmail.com>
2023-12-09 23:34:22 -0600

    adding comments

--- src/test/scala/common/ConvTestPats.scala
+++ src/test/scala/common/ConvTestPats.scala
@@ -4,9 +4,19 @@ import scala.collection.mutable.ListBuffer
 import scala.util.Random
 import org.scalatest.flatspec.AnyFlatSpec
 
+/**
+ * ConvTestPats includes parameters and pre-generated test patterns for V2FConvSpec and F2VConvSpec
+ *
+ */
 object ConvTestPats {
   val rnd = new Random()
 
+  // header specific definition: 4-bit header (sign + 3-bit length)
+  def lutLen(n: Int): Int = if (n == 7) 9 else n // 4b header coding
+
+  def posHeader(v: Int): Int = v | 8 // MSB is negated sign bit (8 means the payload is positive)
+
+  // below are parameters for spec
   val packetbw = 4 // packet bitwidth, hold a header or payload
   val fdbusbw = 128 // bus bitwidth for fixed-size data (fd)
   val vencbusbw = 36 // bus bitwidth for variable-size encoding data (vd)
@@ -27,8 +37,6 @@ object ConvTestPats {
   val testpat_rnd = List.tabulate(ntestpatlen)(_ => rnd.nextInt(1 << (packetbw - 1))) //
 
   val testpatterns = List(testpat_l0, testpat_l1, testpat_l2, testpat_l3, testpat_seq, testpat_rnd)
-
-  def lutLen(n: Int): Int = if (n == 7) 9 else n // 4b header coding
 
   def calcnpacketsforpat(tp: List[Int]): Int = tp.map { v => lutLen(v) + 1 }.reduce(_ + _)
 
@@ -51,7 +59,7 @@ object ConvTestPats {
 
     pat.foreach { v =>
       // fill even overflow
-      val header = v | 8 // MSB is negated sign bit (8 means the payload is positive)
+      val header = posHeader(v)
       buf = buf | (BigInt(header) << (pos * packetbw))
       val len = lutLen(v)
       for ((v, i) <- genpayloadspat(len).zipWithIndex) {
@@ -73,19 +81,19 @@ object ConvTestPats {
     outbufs.toList
   }
 
+}
 
-  class LocalTest extends AnyFlatSpec {
-    behavior of "ConvTestPats"
-    "Test genfixedfrompat" should "pass" in {
-      def testrepreatpat(tp: List[Int]): Unit = {
-        val res: List[BigInt] = genfixedfrompat(tp)
-        val n = fdbusbw / packetbw
-        val m = calcnpacketsforpat(tp)
-        assert(res.length == (m + n - 1) / n) // check the number of the outputs
-        // for (elem <- res)   println(f"out${m}: ${biginthexstr(elem, fdbusbw).reverse}")
-      }
-
-      for (tp <- testpatterns) testrepreatpat(tp)
+class LocalTest extends AnyFlatSpec {
+  import ConvTestPats._
+  behavior of "ConvTestPats"
+  "Test genfixedfrompat" should "pass" in {
+    def testrepreatpat(tp: List[Int]): Unit = {
+      val res: List[BigInt] = genfixedfrompat(tp)
+      val n = fdbusbw / packetbw
+      val m = calcnpacketsforpat(tp)
+      assert(res.length == (m + n - 1) / n) // check the number of the outputs
+      // for (elem <- res)   println(f"out${m}: ${biginthexstr(elem, fdbusbw).reverse}")
     }
+    for (tp <- testpatterns) testrepreatpat(tp)
   }
 }
